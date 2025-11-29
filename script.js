3e07c1acdd7a2f66ffac8c20f241e34221e79e47
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-11-29 21:40:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActive();
+    panel.classList.add('active');
+  });
+});
+
+function removeActive() {
+  panels.forEach(p => p.classList.remove('active'));
+}
+
